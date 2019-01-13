14d63fd89543133a010e17683303c7c68906dd79
VaishakVellore <vaishak.vellore@gmail.com>
2019-01-13 13:35:11 -0700

    Adding String Manipulation Problem Solution

---
+++ src/Arrays_Strings/String_Manipulation.java
@@ -0,0 +1,43 @@
+package Arrays_Strings;
+
+public class String_Manipulation {
+
+    public static String replaceSpaces(char[] s,int length)
+    {
+        int spaceCount = 0, index;
+        for(int i = 0; i<length;i++){
+            if(s[i] == ' ')
+                spaceCount++;
+        }
+
+        index = length + spaceCount*2;
+
+        if(length < s.length)
+            s[length] = '\0';
+        for(int i = length-1; i>=0 ; i--)
+        {
+            if(s[i] == ' ')
+            {
+                s[index - 1] = '0';
+                s[index - 2] = '2';
+                s[index - 3] = '%';
+                index = index - 3;
+            }
+            else
+            {
+                s[index - 1] = s[i];
+                index--;
+            }
+        }
+        return s.toString();
+
+    }
+
+    public static void main(String[] args) {
+         int trueLength = 13;
+         String str = "Mr John Smith    ";
+         char[] s = str.toCharArray();
+         String output = replaceSpaces(s,trueLength);
+         System.out.println(output);
+    }
+}
