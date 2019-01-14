5399a3c8710c5189270c7326844665b70bd04b95
VaishakVellore <vaishak.vellore@gmail.com>
2019-01-13 20:20:05 -0700

    Adding Solution for Zero Matrix Problem

--- src/Arrays_Strings/ZeroMatrix.java
+++ src/Arrays_Strings/ZeroMatrix.java
@@ -4,5 +4,51 @@ package Arrays_Strings;
 
 public class ZeroMatrix {
 
+    static void nullifyRows(int[][] matrix, int row)
+    {
+        for(int j = 0; j<matrix[0].length; j++)
+            matrix[row][j] = 0;
+    }
 
+    static void nullifyColumn(int[][] matrix, int column)
+    {
+        for(int i = 0; i<matrix.length; i++)
+            matrix[i][column] = 0;
+    }
+
+    static int[][] zeroMatrix(int[][] matrix)
+    {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+
+        for(int i =0 ; i<matrix.length; i++)
+        {
+            for(int j = 0; j<matrix[0].length; j++)
+            {
+                if(matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i<row.length; i++){
+            if(row[i]) nullifyRows(matrix, i);
+        }
+
+        for(int j = 0; j<column.length; j++){
+            if(column[j]) nullifyColumn(matrix, j);
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{0,1,2},
+                          {1,2,3},
+                          {4,5,6}};
+
+        int result[][] = zeroMatrix(matrix);
+        System.out.println(result);
+    }
 }
