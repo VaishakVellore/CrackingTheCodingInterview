a9fc8ac9b63c20a889aae448e5e93c392893bd50
VaishakVellore <vaishak.vellore@gmail.com>
2019-01-13 20:26:14 -0700

    Adding String Rotation Problem

---
+++ src/Arrays_Strings/StringRotation.java
@@ -0,0 +1,7 @@
+/*StringRotation:Assumeyouhaveamethodi5Sub5tringwhichchecksifonewordisasubstring of another. Given two strings, 51 and 52, write code to check if 52 is a rotation of 51 using only one call to i5Sub5tring (e.g., "waterbottle" is a rotation of" erbottlewat").
+ * */
+
+package Arrays_Strings;
+
+public class StringRotation {
+}
