7d7aeda8c26c6ddeb9ec7339d8543bd3193d1d8d
VaishakVellore <vaishak.vellore@gmail.com>
2019-01-13 19:39:52 -0700

    Adding Solution of String Compression

--- src/Arrays_Strings/StringCompression.java
+++ src/Arrays_Strings/StringCompression.java
@@ -3,4 +3,28 @@
 package Arrays_Strings;
 
 public class StringCompression {
+
+    static String compressedString(String str)
+    {
+        StringBuilder compressed = new StringBuilder();
+        int countConsecutive = 0;
+
+        for(int i =0; i< str.length();i++)
+        {
+            countConsecutive++;
+            if(i+1 >= str.length() || str.charAt(i) != str.charAt(i+1))
+            {
+                compressed.append(str.charAt(i));
+                compressed.append((countConsecutive));
+                countConsecutive = 0;
+            }
+        }
+        return compressed.length() < str.length()? compressed.toString():str;
+    }
+
+    public static void main(String[] args) {
+        String str = "aabcccccaaa";
+        String output = compressedString(str);
+        System.out.println(output);
+    }
 }
