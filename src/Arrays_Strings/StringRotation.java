ecdc802e32c9821a40ae0a6d9affb49b624785ec
VaishakVellore <vaishak.vellore@gmail.com>
2019-01-13 20:39:44 -0700

    Adding String Rotation Problem

--- src/Arrays_Strings/StringRotation.java
+++ src/Arrays_Strings/StringRotation.java
@@ -4,4 +4,40 @@
 package Arrays_Strings;
 
 public class StringRotation {
+
+    static boolean isRotation(String s1, String s2, int rotation)
+    {
+        if(s1.length() != s2.length())
+            return false;
+
+        StringBuilder x = new StringBuilder();
+        StringBuilder y = new StringBuilder();
+        for( int i =0; i<= rotation; i++)
+        {
+            x.append(s1.charAt(i));
+        }
+
+        for( int i =rotation; i<=s1.length()-1; i++)
+        {
+            y.append(s1.charAt(i));
+        }
+        y = y.append(x);
+        String result = y.toString();
+
+        for(int i = 0; i<result.length()-1;i++){
+            if(result.charAt(i) != s2.charAt(i) )
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "waterbottle";
+        String s2 = "rbottlewate";
+        int rotation = 4;
+
+        boolean output = isRotation(s1,s2, rotation);
+        System.out.println(output);
+    }
 }
